54b1882319c560929ab2d8644be5b84356fbc742
Iris <iris.thuo@student.moringaschool.com>
2023-01-26 13:36:23 +0300

    Header

---
+++ src/Components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <div>
+   
+    <h1
+      style={{
+        borderBottom: "1px solid #E7E7E7",
+        background: '#000',
+        color: "#b8c0ff",
+        textAlign: "center",
+        marginTop: "0px",
+        marginBottom: "10px",
+        padding: "10px"
+      }}
+    >
+      STELLAR SPACE
+    </h1>
+    </div>
+  );
+}
+
+export default Header;
