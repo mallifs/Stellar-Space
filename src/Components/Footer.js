dd4ff2b2439ee5e7f1fe7ac8140d9cc0985f796e
Iris <iris.thuo@student.moringaschool.com>
2023-01-26 13:37:19 +0300

    Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,23 @@
+
+import React from "react";
+
+
+
 function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    );
-}
+    return (       
+      <h3 style={{ 
+        borderTop: "1px solid #E7E7E7",
+        //position: "fixed",
+        left: "0",
+                   padding: "10px",
+                   background: "#000",
+                   color: "#b8c0ff", 
+                   textAlign: "center", 
+                   marginTop: "10px" ,
+                   marginBottom: "0px"}}>
+        STELLAR SPACE: Visualise Space Activities
+      </h3>   
+  );
+};
 
 export default Footer;
